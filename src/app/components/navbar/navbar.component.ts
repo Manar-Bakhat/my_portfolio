d03b98fa61345c1e0f0c4c410e4882a6c59a7f1d
Manar-Bakhat <manarbakhatt@gmail.com>
2024-11-05 20:33:47 +0100

    Responsive Navbar+Home

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,8 +9,13 @@ import { CommonModule } from '@angular/common';  // Importation nécessaire pour
   styleUrls: ['./navbar.component.css']  // Assurez-vous que c'est 'styleUrls' et non 'styleUrl'
 })
 export class NavbarComponent {
-
+  isMenuOpen = false;  // Pour gérer l'état du menu burger
   isSticky: boolean = false;
+
+  // Fonction pour ouvrir/fermer le menu burger
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
