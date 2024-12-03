a53b44b595eedf8fde13750802cecff7b24b49f7
Manar-Bakhat <manarbakhatt@gmail.com>
2024-12-03 01:59:19 +0100

    Make home Responsive

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { AboutComponent } from '../about/about.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { EduExpComponent } from '../../edu-exp/edu-exp.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AboutComponent, SkillsComponent, EduExpComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/edu-exp/edu-exp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edu-exp',
+  standalone: true,
+  imports: [],
+  templateUrl: './edu-exp.component.html',
+  styleUrl: './edu-exp.component.css'
+})
+export class EduExpComponent {
+
+}
